7a288cece08ddffd7df0b5963752806f0b3c7504
liupengcheng <liupengcheng@wtoip.com>
2019-03-20 11:06:15 +0800

    helloworld

---
+++ roc-demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+/**
+ * ClassName: TestController
+ *
+ * @author liupengcheng
+ * @Date 2019/3/20
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public static String test(){
+        return "helloWorld";
+    }
+
+}
