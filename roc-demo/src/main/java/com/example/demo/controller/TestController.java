691af5ff375484fd4771b995aebd911eb2114a24
liupengcheng <liupengcheng@deepexi.com>
2019-08-15 17:14:39 +0800

    777

--- roc-demo/src/main/java/com/example/demo/controller/TestController.java
+++ roc-demo/src/main/java/com/example/demo/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
     public static String test(){
 		String a = "456";
 		String b = "789";
-		String c = "999";
+		String c = "777";
         return "helloWorld";
     }
 
